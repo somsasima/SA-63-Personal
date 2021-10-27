27ff8290c02aab941436167b7f09f2bb33b727a7
somsasima <sasima.atw@hotmail.com>
2021-10-27 13:36:14 +0700

    no node

--- frontend/node_modules/@backstage/plugin-identity-backend/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { Logger } from 'winston';
-
-interface RouterOptions {
-    logger: Logger;
-}
-declare function createRouter(options: RouterOptions): Promise<express.Router>;
-
-export { RouterOptions, createRouter };
--- frontend/node_modules/@backstage/plugin-scaffolder-backend/README.md
+++
@@ -1,6 +0,0 @@
-# Title
-Welcome to the scaffolder plugin!
-
-## Sub-section 1
-
-## Sub-section 2
